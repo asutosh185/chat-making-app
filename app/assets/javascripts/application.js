9eef5b2d3238d62d4210e39adfb40e5fda8e995d
asutosh1993 <asutosh@gurujada.com>
2019-02-11 09:58:55 +0000

    add navigation and dropdown jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,19 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require semantic-ui
+//= require semantic-ui/modal
+//= require semantic-ui/dropdown
 //= require_tree .
+
+$(document).on('turbolinks:load',function(){
+    $('.ui.dropdown').dropdown()
+;
+})
+
+
+
