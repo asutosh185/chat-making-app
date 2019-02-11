4c6bfd836c5f5ba7cb43928314cc7aff922f774b
asutosh1993 <asutosh@gurujada.com>
2019-02-11 12:48:20 +0000

    iterator applied and login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "asutosh", password: "password")   
+User.create(username: "bapun", password: "password1")   
+User.create(username: "asutosh123", password: "password123")   
+User.create(username: "asutosh456", password: "password456")   
+User.create(username: "asutosh789", password: "password789")   
