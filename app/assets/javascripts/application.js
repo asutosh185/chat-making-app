d928c9edd1e60d55dea67eccfb825b7ba5abaec8
asutosh1993 <asutosh@gurujada.com>
2019-02-11 21:35:30 +0000

    scroll buttom and scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,12 @@
 //= require semantic-ui/dropdown
 //= require_tree .
 
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on('turbolinks:load',function(){
     $('.ui.dropdown').dropdown();
 $('.message .close')
@@ -29,6 +35,7 @@ $('.message .close')
     ;
   })
 ;
+ scroll_bottom();
 })
 
 
